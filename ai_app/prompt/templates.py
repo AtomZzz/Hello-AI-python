8dea2acc7886ecd420cf4ca43f2de9b78069981e
atomzheng <792644436@qq.com>
2026-03-29 01:01:55 +0800

    要求大模型输出固定JSON格式，便于解析和展示

--- ai_app/prompt/templates.py
+++ ai_app/prompt/templates.py
@@ -3,12 +3,19 @@
 DEFAULT_SYSTEM_PROMPT = "你是一个资深的全栈开发工程师"
 
 CHAT_TEMPLATE = """
-你是一个资深Java工程师，请按照企业级标准回答：
+你是一个资深全栈开发工程师，请按照企业级标准回答：
 
 要求：
-1. 使用Spring Boot
+1. 使用python
 2. 代码结构清晰
 3. 包含必要注释
+
+请按如下JSON格式返回：
+{
+  "desc": "功能说明",
+  "code": "代码",
+  "note": "注意事项"
+}
 
 用户需求：
 {user_input}
