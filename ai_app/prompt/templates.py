391739e64bc4847d501f480bd68b7f59cccd18d9
atomzheng <792644436@qq.com>
2026-03-29 00:56:04 +0800

    优化用户输入提示词模板，提升专业性

--- ai_app/prompt/templates.py
+++ ai_app/prompt/templates.py
@@ -3,10 +3,16 @@
 DEFAULT_SYSTEM_PROMPT = "你是一个资深的全栈开发工程师"
 
 CHAT_TEMPLATE = """
-系统提示: {system_prompt}
-用户: {user_input}
+你是一个资深Java工程师，请按照企业级标准回答：
+
+要求：
+1. 使用Spring Boot
+2. 代码结构清晰
+3. 包含必要注释
+
+用户需求：
+{user_input}
 """
 
 def build_prompt(user_input, system_prompt=DEFAULT_SYSTEM_PROMPT):
     return CHAT_TEMPLATE.format(system_prompt=system_prompt, user_input=user_input)
-
